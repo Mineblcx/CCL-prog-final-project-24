ac747a279c8adea0e1602f7e0bd0f5218a47a306
Blox <161893631+Mineblcx@users.noreply.github.com>
2024-05-23 16:26:04 -0400

    Progression

--- projet final 2024/Game/game.js
+++ projet final 2024/Game/game.js
@@ -1,44 +1,39 @@
 //play area
+let Running;
+let UnBought;
+let Bought;
 const canvas = document.getElementById("gameCanvas");
-
-//players
-var player_1 = {
-  name: "Blue Player",
-  Gold_Ammount: {
-    if(Level_0 = False) {
-      if (Level_1 = False) {
-        if (Level_2 = False) {
-          if (Level_3 = False) {
-            if (Level_4 = False) {
-              Gold_Ammount = Level_5
-            }
-            else {
-              Level_4
-            }
+function GameLoad(x){
+  if(x = on){
+  let GameStatus = Running;
+  }
+  else{
+    console.log(error);
+  }
+}
+function ButtonTest() {
+  if (Level_1 = UnBought) {
+    Level_1 = Bought
+  }
+  else {
+    if (Level_2 = UnBought) {
+      Level_2 = Bought
+    }
+    else {
+      if (Level_3 = UnBought) {
+        Level_3 = Bought
+      } else {
+        if (Level_4 = UnBought) {
+          Level_4 = Bought
+        } else {
+          if (Level_5 = UnBought) {
+            Level_5 = Bought
           }
-          else {
-            Level_3
-          }
-
-        }
-        else {
-          Level_2
         }
       }
-      else {
-        Level_1
-      }
-      else {
-      //
     }
-    }
-
-  },
-};
-
-
-// Library Legends
-
+  }
+}
 class Unit {
   constructor(name, hp, atkdmg, atkspd, atkrng, WalkSPD, cost, killgain, spwammount, spwtimer) {
     //name, Health Points, Attack Dammage, Attack speed, Attack range (pixels), Walk speed(Pixels per seconds), cost, money gained from enemy kills, spawn ammount, spawn timer
@@ -61,22 +56,56 @@ class Gold {
   }
 }
 
+while(GameStatus = Running){
+//Cash library
+const Level_0_stats = new Gold(5, 0);
+const Level_1_stats = new Gold(10, 150);
+const Level_2_stats = new Gold(15, 450);
+const Level_3_stats = new Gold(20, 1125);
+const Level_4_stats = new Gold(25, 1050);
+const Level_5_stats = new Gold(30, 2000);
+let Level_1 = 0;
+let Level_2 = 0;
+let Level_3 = 0;
+let Level_4 = 0;
+let Level_5 = 0;
+
+//players
+var player_1 = {
+  name: "Blue Player",
+  Gold_Amount: {
+    if(Level_5 = UnBought) {
+      if (Level_4 = UnBought) {
+        if (Level_3 = UnBought) {
+          if (Level_2 = UnBought) {
+            if (Level_1 = UnBought) {
+              console.log(good0);
+            } else {
+              console.log(good1);
+            }
+          } else {
+            console.log(good2);
+          }
+        } else {
+          console.log(good3);
+        }
+      } else {
+        console.log(good4);
+      }
+    }, else: {
+
+    }
+  },
+}
+
 // Unit Library
 const Newbie = new Unit("Newbie", 1000, 200, 1.5, 50, 100, 50, 10, 5, 3);
 const Knight = new Unit("Knight", 1500, 300, 1.5, 40, 60, 125, 45, 3, 5);
 const Assasin = new Unit("Assasin", 1250, 400, 0.75, 35, 150, 500, 25, 5, 4);
 const Sword = new Unit("Sword Master", 4000, 500, 1, 75, 125, 1500, 350, 2, 7.5);
 
-
-//Cash library
-const Level_0 = new Gold(5, 0);
-const Level_1 = new Gold(10, 150);
-const Level_2 = new Gold(25, 450);
-const Level_3 = new Gold(35, 1125);
-const Level_4 = new Gold(40, 1050);
-const Level_5 = new Gold(50, 2000);
-
 // other libraries
 const baseHP = 30000;
 var time1 = new Date();
 delta_time = time1.getTime();
+}
