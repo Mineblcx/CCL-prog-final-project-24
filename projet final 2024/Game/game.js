d6ee3e948f6534e5a88f2ab94dbb06093f3c900a
Blox <161893631+Mineblcx@users.noreply.github.com>
2024-05-28 16:21:01 -0400

    progress

--- projet final 2024/Game/game.js
+++ projet final 2024/Game/game.js
@@ -1,39 +1,64 @@
+
+
 //play area
-let Running;
-let UnBought;
-let Bought;
 const canvas = document.getElementById("gameCanvas");
-function GameLoad(x){
-  if(x = on){
-  let GameStatus = Running;
+console.log("should work");
+
+function GameLoad(x) {
+  if (x = 'on') {
+    return GameStatus = "Running";
   }
-  else{
-    console.log(error);
+  else {
+    return GameStatus = "Stoped";
+    console.log("error");
   }
 }
 function ButtonTest() {
-  if (Level_1 = UnBought) {
-    Level_1 = Bought
+  if (Level_1 = "UnBought") {
+    Level_1 = "Bought";
   }
   else {
-    if (Level_2 = UnBought) {
-      Level_2 = Bought
+    if (Level_2 = "UnBought") {
+      Level_2 = "Bought";
     }
     else {
-      if (Level_3 = UnBought) {
-        Level_3 = Bought
+      if (Level_3 = "UnBought") {
+        Level_3 = "Bought";
       } else {
-        if (Level_4 = UnBought) {
-          Level_4 = Bought
+        if (Level_4 = "UnBought") {
+          Level_4 = "Bought";
         } else {
-          if (Level_5 = UnBought) {
-            Level_5 = Bought
+          if (Level_5 = "UnBought") {
+            Level_5 = "Bought";
           }
         }
       }
     }
   }
+  moneyfunct();
 }
+function moneyfunct() {      if(Level_5 = "UnBought") {
+        if (Level_4 = "UnBought") {
+          if (Level_3 = "UnBought") {
+            if (Level_2 = "UnBought") {
+              if (Level_1 = "UnBought") {
+                console.log("good0");
+              } else {
+                console.log("good1");
+              }
+            } else {
+              console.log("good2");
+            }
+          } else {
+            console.log("good3");
+          }
+        } else {
+          console.log("good4");
+        }
+      } else {
+
+      }
+    }
 class Unit {
   constructor(name, hp, atkdmg, atkspd, atkrng, WalkSPD, cost, killgain, spwammount, spwtimer) {
     //name, Health Points, Attack Dammage, Attack speed, Attack range (pixels), Walk speed(Pixels per seconds), cost, money gained from enemy kills, spawn ammount, spawn timer
@@ -55,8 +80,14 @@ class Gold {
     this.price = price;
   }
 }
-
-while(GameStatus = Running){
+class Player {
+  constructor(name, gold, troopamnt, color){
+    this.name = name;
+    this.gold = gold;
+    this.troopamnt = troopamnt;
+    this.color = color;
+  }
+}
 //Cash library
 const Level_0_stats = new Gold(5, 0);
 const Level_1_stats = new Gold(10, 150);
@@ -64,48 +95,23 @@ const Level_2_stats = new Gold(15, 450);
 const Level_3_stats = new Gold(20, 1125);
 const Level_4_stats = new Gold(25, 1050);
 const Level_5_stats = new Gold(30, 2000);
-let Level_1 = 0;
-let Level_2 = 0;
-let Level_3 = 0;
-let Level_4 = 0;
-let Level_5 = 0;
+let Level_1 = "UnBought";
+let Level_2 = "UnBought";
+let Level_3 = "UnBought"; 
+let Level_4 = "UnBought";
+let Level_5 = "UnBought";
+//while (GameStatus = "Running") {
+  //players
+  const Player_1 = new Player("Player 1", 0, "Blue")
 
-//players
-var player_1 = {
-  name: "Blue Player",
-  Gold_Amount: {
-    if(Level_5 = UnBought) {
-      if (Level_4 = UnBought) {
-        if (Level_3 = UnBought) {
-          if (Level_2 = UnBought) {
-            if (Level_1 = UnBought) {
-              console.log(good0);
-            } else {
-              console.log(good1);
-            }
-          } else {
-            console.log(good2);
-          }
-        } else {
-          console.log(good3);
-        }
-      } else {
-        console.log(good4);
-      }
-    }, else: {
+  // Unit Library
+  const Newbie = new Unit("Newbie", 1000, 200, 1.5, 50, 100, 50, 10, 5, 3);
+  const Knight = new Unit("Knight", 1500, 300, 1.5, 40, 60, 125, 45, 3, 5);
+  const Assasin = new Unit("Assasin", 1250, 400, 0.75, 35, 150, 500, 25, 5, 4);
+  const Sword = new Unit("Sword Master", 4000, 500, 1, 75, 125, 1500, 350, 2, 7.5);
 
-    }
-  },
-}
-
-// Unit Library
-const Newbie = new Unit("Newbie", 1000, 200, 1.5, 50, 100, 50, 10, 5, 3);
-const Knight = new Unit("Knight", 1500, 300, 1.5, 40, 60, 125, 45, 3, 5);
-const Assasin = new Unit("Assasin", 1250, 400, 0.75, 35, 150, 500, 25, 5, 4);
-const Sword = new Unit("Sword Master", 4000, 500, 1, 75, 125, 1500, 350, 2, 7.5);
-
-// other libraries
-const baseHP = 30000;
-var time1 = new Date();
-delta_time = time1.getTime();
-}
+  // other libraries
+  const baseHP = 30000;
+  var time1 = new Date();
+  delta_time = time1.getTime();
+//};
